da8620386b446759c13f912a25e6c657fed68bf7
Flavia Aparecida dos Santos Silva - RF: 47.093 - SEDUC <flaviassi@praiagrande.sp.gov.br>
2022-04-18 14:13:55 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,14 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+}
