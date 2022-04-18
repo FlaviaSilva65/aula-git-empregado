d88bf410281c73f42429aa8d1090152a194a783d
Flavia Aparecida dos Santos Silva - RF: 47.093 - SEDUC <flaviaass65@gmail.com>
2022-04-18 15:52:38 -0300

    Primeiro teste concluido com sucesso

--- Empregado.java
+++ Empregado.java
@@ -1,4 +1,4 @@
-public class Empregado{
+public class Empregado {
     private String nome;
     private int idade;
     private int tipo;
@@ -6,28 +6,61 @@ public class Empregado{
     private double comissao;
     private double bonus;
 
-    public double calculaSalario(){
-        if(tipo == 1){
+    public double calculaSalario() {
+        if (tipo == 1) {
             return salario;
-        }else if(tipo == 2){
+        } else if (tipo == 2) {
             return salario + salario * comissao;
-        }else if(tipo == 3){
+        } else if (tipo == 3) {
             return salario + bonus;
-        }else{
+        } else {
             return 0;
         }
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
-    public void setNome(String nome){
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    public int getIdade(){
+
+    public int getIdade() {
         return this.idade;
     }
-    public void setIdade(int idade){
+
+    public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int tipo() {
+        return this.tipo;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setComissao(double comissao){
+        this.comissao = comissao;
+    }
+
+    public double getComissao(){
+        return this.comissao;
+    }
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+    public double getBonus(){
+        return this.bonus;
     }
 }
